fcd29e42cb4ed53533a196a2da4b0e3206af0c5b
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-27 00:20:14 -0400

    Create and Design Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiFillLinkedin} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <section id = 'footer'>
-    </section>
+    <footer>
+      <a href="#" className='footer__logo'>RAGHAD ASFOUR</a>
+
+      <ul className="permalinks">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#experience">EXPERIENCE</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://github.com/raghadasfour-02"><AiFillGithub/></a>
+        <a href="https://www.linkedin.com/in/raghadasfour/"><AiFillLinkedin/></a>
+      </div>
+
+      <div className='footer_copyright'>
+        &copy; Coded and Designed by Raghad Asfour
+      </div>
+    </footer>
   )
 }
 
