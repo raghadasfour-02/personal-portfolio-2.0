4b0bf04da069b599e962c2b7653abc0c67377aa9
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-26 23:17:46 -0400

    Integrate EmailJs

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -35,13 +35,12 @@ const Contact = () => {
 
         <form action="">
           <input type="text" name="name" placeholder='Your Full Name' required />
-          <input type="email" name="name" placeholder="Your Email" required />
+          <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows='7' placeholder="Your Message" required></textarea>
           <button type='submit' className='btn btn-primary' >Send Message</button>
         </form>
 
       </div>
-
     </section>
   )
 }
