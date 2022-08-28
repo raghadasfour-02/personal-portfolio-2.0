e36a3606fa4f0efc1925407d3c089e0b9014d369
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-28 01:57:20 -0400

    Create Experience Section

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 const Experience = () => {
   return (
@@ -9,14 +10,75 @@ const Experience = () => {
 
       <div className="container experience__container">
         <div className="experience__work">
+          <h3>Work Experience</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Software Developer</h4>
+              <small className='text-light'>DoctorCare</small>
+              <h5>Python, Django, SQL, AWS</h5>
+              </div>
+            </article>
 
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Software Developer</h4>
+              <small className='text-light'>DoctorCare</small>
+              <h5>Python, Django, SQL</h5>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Web Developer</h4>
+              <small className='text-light'>Netact Inc.</small>
+              </div>
+            </article>
+          </div>
         </div>
 
-        <div className="experience__project">
+        <div className="experience__projects">
+          <h3>Projects</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Personal Portfolio</h4>
+              <h5>React</h5>
+              </div>
+            </article>
 
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Movie Critic Blog</h4>
+              <h5>Python, Django</h5>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>News Aggregator</h4>
+              <h5>Python, Django</h5>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              < AiOutlineArrowRight className='experience__details-icon' />
+              <div>
+              <h4>Food Suggester</h4>
+              <h5>React, Javascript</h5>
+              </div>
+            </article>
+
+          </div>
         </div>
+
       </div>
-
     </section>
   )
 }
