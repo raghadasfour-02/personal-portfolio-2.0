9bc60e98170fbcf1f5f05fc4693f34ce5a1dfb72
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-28 15:51:11 -0400

    Make Experience Section Responsive

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,8 +6,8 @@ const About = () => {
     <section id = 'about'>
       <h5>Get to Know Me</h5>
       <h1>ABOUT ME</h1>
+      
       <div className="container about__container">
-
         <div className="about__content">
           <div className="about_cards">
             <article className='about__card'>
