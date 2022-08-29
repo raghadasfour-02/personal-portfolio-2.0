d1dcae42c4f635ad0a364c301d39205897677182
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-29 00:13:21 -0400

    Implement Floating Objects

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './header.css'
 import HeaderSocials from './HeaderSocials'
 import Programmer from '../../assets/programmer.png'
+import aws from '../../assets/aws.png'
+import django from '../../assets/django.png'
+import python from '../../assets/python.png'
+import javascript from '../../assets/javascript.png'
+import cpp from '../../assets/cpp.png'
 
 const Header = () => {
   return (
@@ -11,6 +16,17 @@ const Header = () => {
         <h1>Raghad Asfour</h1>
         <h4 className='text-light'>Welcome to my Portfolio.</h4>
         <HeaderSocials/>
+
+        <div className='react-floater-animated'>
+          <img id="aws" src={aws} alt="aws"/>
+          <img id="cpp" src={cpp} alt="cpp"/>
+          <img id="javascript" src={javascript} alt="javascript"/>
+        </div>
+
+        <div className='react-floater-animated-opposite'>
+          <img id="python" src={python} alt="python"/>
+          <img id="django" src={django} alt="django"/>
+        </div>
 
         <div className='programmer_image'>
           <img src={Programmer} alt="PROGRAMMER" />
