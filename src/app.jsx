b8bd424533cdfb278cfb8b5a803f131b783beb8f
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-09-11 20:31:44 -0400

    Install GSAP Package

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import Header from './components/header/Header'
 import Experience from './components/experience/Experience'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import { gsap } from "gsap";
 
 const app = () => {
   return (
