b7abd948dab69003b50123d47384ecc711656fc2
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-09-25 19:25:54 -0400

    Animate Header Fade-In

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import HeaderSocials from './HeaderSocials'
+import Anime from 'react-anime';
 
 const Header = () => {
   return (
@@ -8,9 +9,15 @@ const Header = () => {
       <div className="container header__container">
         <HeaderSocials/>
         <div className='centered_header'>
-        <h5 className='medium_text'>Hello, I'm</h5>
+        <Anime opacity={[0, 1]} translateY={'-1em'} easing={'easeInBack'} delay={100}>
+          <h5 className='medium_text'>Hello!</h5>
+        </Anime>
+        <Anime opacity={[0, 1]} translateY={'-1em'} easing={'easeInBack'} delay={400}>
         <h1 className='large_text'>Raghad Asfour</h1>
+        </Anime>
+        <Anime opacity={[0, 1]} translateY={'-1em'} easing={'easeInBack'} delay={1000}>
         <h4 className='text-light' className='medium_text'>Welcome to my Portfolio.</h4>
+        </Anime>
         </div>
         <a href = '#contact' className = 'scroll__down'> ------------> </a>
       </div>
