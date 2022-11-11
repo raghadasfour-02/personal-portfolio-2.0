b0aac873366acbfc32ec344120415f11001cd0c7
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-11-11 12:31:37 -0500

    Remove Deprecated Code

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,9 +8,9 @@ const HeaderSocials = () => {
     <div className='header__socials'>
         <div class="social-menu">
         <ul>
-            <li><a href="https://www.linkedin.com/in/raghadasfour" target="_blank"><AiFillLinkedin size={30} color="#0a192f"/></a></li>
-            <li><a href="https://github.com/raghadasfour-02" target="_blank"><AiFillGithub size={30} color="#0a192f"/></a></li>
-            <li><a href="mailto: raghadasfour.74@gmail.com"target="_blank"><AiOutlineMail size={30} color="#0a192f"/></a></li>
+            <li><a href="https://www.linkedin.com/in/raghadasfour/"><AiFillLinkedin size={30} color="#0a192f"/></a></li>
+            <li><a href="https://github.com/raghadasfour-02"><AiFillGithub size={30} color="#0a192f"/></a></li>
+            <li><a href="mailto: raghadasfour.74@gmail.com"><AiOutlineMail size={30} color="#0a192f"/></a></li>
         </ul>
         </div>
     </div>
