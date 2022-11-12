1f185828d563a3bc677f95f9e6f0eb37fd60c71f
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-11-12 11:48:32 -0500

    Reorganize 'Experience' Section

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -7,31 +7,41 @@ const Experience = () => {
     <section id = 'experience'>
       <h5>Things I Know + Places I've Worked at</h5>
       <h1>EXPERIENCE</h1>
-
-      <div className="container experience__container">
-        <div className="experience__work">
-          <h4>DoctorCare (May 2022 - Aug 2022)</h4>
-            <article className='experience__details'>
-              < AiOutlineArrowRight className='experience__details-icon' />
-              <h4>Software Developer</h4>
-              <h4 style={{color: "var(--color-neon-green)"}}>Python, Django, SQL, AWS</h4>
+      <div className="container about__container">
+        <div className="about__content">
+          <div className="about_cards">
+          <h4>DOCTORCARE</h4>
+            <article className='about__card'>
+              <h5>Software Developer</h5>
+              <small>May 2022 - Aug 2022 <br /> 
+              Tools: Python, Django, PostgreSQL, AWS
+              </small>
             </article>
 
-          <br></br>
-          <h4>DoctorCare (Sept 2021 - Dec 2021)</h4>
-            <article className='experience__details'>
-              < AiOutlineArrowRight className='experience__details-icon' />
-              <h4>Software Developer</h4>
-              <h4 style={{color: "var(--color-neon-green)"}}>Python, Django, SQL</h4>
+            <h4>DOCTORCARE</h4>
+            <article className='about__card'>
+              <h5>Software Developer</h5>
+              <small>Sept 2021 - Dec 2021 <br /> 
+              Tools: Python, Django, PostgreSQL
+              </small>
             </article>
-          
-          <br></br>
-          <h4>Netact Inc (Jan 2021 - Apr 2021)</h4>
-            <article className='experience__details'>
-              < AiOutlineArrowRight className='experience__details-icon' />
-              <h4>Web Developer</h4>
-              <h4 style={{color: "var(--color-neon-green)"}}>Javascript, HTML, CSS</h4>
+
+            <h4>NETACT SERVICES INC</h4>
+            <article className='about__card'>
+              <h5>Web Developer</h5>
+              <small>Jan 2021 - Apr 2021 <br /> 
+              Tools: Javascript, HTML, CSS
+              </small>
             </article>
+
+            <h4>TRILLIUM HEALTH PARTNERS</h4>
+            <article className='about__card'>
+              <h5>Web Developer</h5>
+              <small>Jan 2021 - Apr 2021 <br /> 
+              Tools: PHP, Javascript, MySQL
+              </small>
+            </article>
+          </div>
         </div>
       </div>
     </section>
