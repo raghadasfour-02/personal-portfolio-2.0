0bca2dbdffcef2c7f74bdee044f68db223e80284
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-11-14 23:54:47 -0500

    Configure Extracurriculars First Slide

--- src/components/extracurriculars/ExtracurricularsCarousel.jsx
+++ src/components/extracurriculars/ExtracurricularsCarousel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import Item from "./Item";
 import "./extracurriculars.css";
+import quantum_mechanics_talk from './quantum_mechanics_talk.jpg';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -15,7 +16,8 @@ function ExtracurricularsCarousel() {
     <div className='carousel_container'>
         <Carousel breakPoints={breakPoints}>
           <Item>
-            <small>FOOD SUGGESTER</small>
+            <small>Quantum Mechanics Talk</small>
+            <img src={quantum_mechanics_talk}></img>
           </Item>
 
           <Item>
