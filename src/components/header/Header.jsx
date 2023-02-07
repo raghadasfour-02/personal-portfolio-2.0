e76155cfee93e6203f25e7137fa2b9b257a0edfc
raghadasfour-02 <r2asfour@uwaterloo.ca>
2023-02-07 15:03:44 -0500

    Update Portfolio Introduction

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
           <h5 className='medium_text'>Hello!</h5>
         </Anime>
         <Anime opacity={[0, 1]} translateY={'-1em'} easing={'easeInBack'} delay={400}>
-        <h1 className='large_text'>Raghad Asfour</h1>
+        <h1 className='large_text'>I'm Raghad Asfour</h1>
         </Anime>
         <Anime opacity={[0, 1]} translateY={'-1em'} easing={'easeInBack'} delay={1000}>
         <h4 className='text-light' className='medium_text'>Welcome to my Portfolio.</h4>
